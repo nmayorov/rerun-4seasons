e6f4214ef872183fa54bd5deed5b38bc5f95148e
Nikolay Mayorov <nikolay.mayorov@zoho.com>
2026-03-07 02:25:01 +0300

    MNT: Use colorgrad for coloring

--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,4 @@
+use colorgrad::Gradient;
 use rerun::external::glam;
 
 pub fn isometry_to_rerun(transform: &nalgebra::Isometry3<f64>) -> rerun::Transform3D {
@@ -18,19 +19,10 @@ pub fn color_range(
     min: f64,
     max: f64,
 ) -> impl Iterator<Item = rerun::Color> {
+    let grad = colorgrad::preset::turbo();
     values.map(move |x| {
         let t = (x - min) / (max - min);
-        select_color_turbo(t as f32)
+        let [r, g, b, _] = grad.at(t as f32).to_rgba8();
+        rerun::Color::from_rgb(r, g, b)
     })
-}
-
-fn select_color_turbo(t: f32) -> rerun::Color {
-    let r = (34.61
-        + t * (1172.33 - t * (10793.56 - t * (33300.12 - t * (38394.49 - t * 14825.05)))))
-        .clamp(0.0, 255.0) as u8;
-    let g = (23.31 + t * (557.33 + t * (1225.33 - t * (3574.96 - t * (1073.77 + t * 707.56)))))
-        .clamp(0.0, 255.0) as u8;
-    let b = (27.2 + t * (3211.1 - t * (15327.97 - t * (27814.0 - t * (22569.18 - t * 6838.66)))))
-        .clamp(0.0, 255.0) as u8;
-    rerun::Color::from_rgb(r, g, b)
 }
